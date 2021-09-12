e0e8408f9527218ab309e7e4d8f892a9839c7417
Jacek Korzemski <wrp40.000@wp.pl>
2021-09-12 19:50:39 +0200

    Frontend version info

--- src/views/Index.js
+++ src/views/Index.js
@@ -5,18 +5,30 @@ const Index = () => {
   return (
     <Page title={"Home page"}>
       <div className="standard-text">
-        <h1>Metal Catalogue - version 0.1.3</h1>
+        <h1>Metal Catalogue - version 0.1.4</h1>
         <h3>Working features:</h3>
         <ul>
-          <li>Browsing new clips</li>
-          <li>Browsing channels</li>
-          <li>Listening to clips</li>
+          <li>
+            New ones:
+            <ul>
+              <li>Registering</li>
+              <li>Loging in</li>
+              <li>Loging out (yeah, it's also very important feature!)</li>
+            </ul>
+          </li>
+          <li>
+            Old ones:
+            <ul>
+              <li>Browsing new clips</li>
+              <li>Browsing channels</li>
+              <li>Listening to clips</li>
+            </ul>
+          </li>
         </ul>
         <h3>Incoming features</h3>
         <ul>
           <li>Searching</li>
           <li>Informations: privacy policy, about app, contact</li>
-          <li>Login/register</li>
           <li>Disabled functionalities for guests</li>
           <li>Add user profiles</li>
           <li>Add user playlists</li>
