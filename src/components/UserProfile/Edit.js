03a6ec770ba94f08b76e2cae6fbbbfa7f3b4e7b7
Jacek Korzemski <wrp40.000@wp.pl>
2021-09-22 14:40:16 +0200

    [frontend] - Handling loading profile

--- src/components/UserProfile/Edit.js
+++ src/components/UserProfile/Edit.js
@@ -4,6 +4,7 @@ import { AppContext } from "components/AppWrapper/AppWrapper";
 import Button from "components/Button/Button";
 import { api_url } from "Config";
 import Channels from "./Channels";
+import Loading from "components/Loading/LoadingBlack";
 
 const Edit = () => {
   const app = useContext(AppContext);
@@ -45,72 +46,80 @@ const Edit = () => {
       >
         [X]
       </span>
-      <form>
-        <h2 style={{ textAlign: "center" }}>Your profile</h2>
-        <hr />
-        <p>Display name:</p>
-        <input type="text" name="display_name" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
-        <p className="section-title">Your subscribed channels:</p>
-        <div className="channels">
-          {data && data.subscribed_channels ? (
-            <>
-              <Channels channels={data?.subscribed_channels} />
-            </>
-          ) : (
-            <>No subscribed channels</>
-          )}
+      {!data ? (
+        <div style={{ height: "300px" }}>
+          <Loading />
         </div>
-        <p className="section-title">Your Playlists:</p>
-        <div className="playlists">
-          {data && data.playlists ? (
-            <>
-              {data.playlists.map((elem, index) => (
-                <div className="playlist" key={index}>
-                  {elem}
-                </div>
-              ))}
-            </>
-          ) : (
-            <p className="blank">No playlists</p>
-          )}
-        </div>
-        <p className="section-title">Your reviews:</p>
-        <div className="reviews">
-          {data && data.reviews ? (
-            <>
-              {data.reviews.map((elem, index) => (
-                <div className="review" key={index}>
-                  {elem}
-                </div>
-              ))}
-            </>
-          ) : (
-            <p className="blank">No reviews</p>
-          )}
-        </div>
-        <p className="section-title">Your votes:</p>
-        <div className="votes">
-          {data && data.votes ? (
-            <>
-              {data.votes.map((elem, index) => (
-                <div className="vote" key={index}>
-                  {elem}
-                </div>
-              ))}
-            </>
-          ) : (
-            <p className="blank">No votes</p>
-          )}
-        </div>
-      </form>
-      <p className="buttons-wrapper">
-        <Button type="button" onClick={() => {}}>
-          Update
-        </Button>
-        <Button type="button" onClick={() => app.setModal(false)}>
-          Close
-        </Button>
-      </p>
+      ) : (
+        <>
+          <form>
+            <h2 style={{ textAlign: "center" }}>Your profile</h2>
+            <hr />
+            <p>Display name:</p>
+            <input type="text" name="display_name" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
+            <p className="section-title">Your subscribed channels:</p>
+            <div className="channels">
+              {data && data.subscribed_channels ? (
+                <>
+                  <Channels channels={data?.subscribed_channels} />
+                </>
+              ) : (
+                <>No subscribed channels</>
+              )}
+            </div>
+            <p className="section-title">Your Playlists:</p>
+            <div className="playlists">
+              {data && data.playlists ? (
+                <>
+                  {data.playlists.map((elem, index) => (
+                    <div className="playlist" key={index}>
+                      {elem}
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <p className="blank">No playlists</p>
+              )}
+            </div>
+            <p className="section-title">Your reviews:</p>
+            <div className="reviews">
+              {data && data.reviews ? (
+                <>
+                  {data.reviews.map((elem, index) => (
+                    <div className="review" key={index}>
+                      {elem}
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <p className="blank">No reviews</p>
+              )}
+            </div>
+            <p className="section-title">Your votes:</p>
+            <div className="votes">
+              {data && data.votes ? (
+                <>
+                  {data.votes.map((elem, index) => (
+                    <div className="vote" key={index}>
+                      {elem}
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <p className="blank">No votes</p>
+              )}
+            </div>
+          </form>
+          <p className="buttons-wrapper">
+            <Button type="button" onClick={() => {}}>
+              Update
+            </Button>
+            <Button type="button" onClick={() => app.setModal(false)}>
+              Close
+            </Button>
+          </p>
+        </>
+      )}
     </EditWrapper>
   );
 };
